3f6024e342b8860bd713f75e2375479bdc1501e4
Hunter O'Toole <90637885+Topzombie22@users.noreply.github.com>
2023-02-21 18:10:03 -0400

    Initial commit

---
+++ Assets/Scripts/PhysicsCharacterController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsCharacterController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private GameObject cam;
+
+    private float xInput;
+    private float zInput;
+    public float xSensativity;
+    public float ySensativity;
+    private float camX;
+    private float camY;
+    private float rotation;
+    public Vector3 speedCap;
+    public float speed;
+    public float rbVelocity;
+    public Vector3 playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        CamMovement();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        zInput = Input.GetAxisRaw("Horizontal");
+        xInput = Input.GetAxisRaw("Vertical");
+        playerInput = new Vector3(zInput, 0f, xInput);
+        if (rbVelocity < 5)
+        {
+            rb.velocity += transform.TransformDirection(playerInput.normalized) * speed * Time.fixedDeltaTime;
+        }
+    }
+
+    void CamMovement()
+    {
+        camX += xSensativity * Input.GetAxis("Mouse X");
+        camY += -ySensativity * Input.GetAxis("Mouse Y");
+
+        camY = Mathf.Clamp(camY, -89f, 89);
+
+        transform.rotation = Quaternion.Euler(0f, camX, 0f);
+        cam.transform.rotation = Quaternion.Euler(camY, camX, 0f);
+    }
+}
